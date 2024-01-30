08d9fb1188cd5b82ce252b7d58d5ff3c1b06e7b4
wrazaq <waleed.razaq2000@gmail.com>
2024-01-30 19:50:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/m8_uf2_projecte_firebase_jxx_wrk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.m8_uf2_projecte_firebase_jxx_wrk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M8_UF2_PROJECTE_FIREBASE_JXX_WRK"
+include(":app")
