6f803b19b1115d347eea2993db75a1175bbfc162
wrazaq <waleed.razaq2000@gmail.com>
2024-02-04 18:39:38 +0100

    created signup.java and xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -39,6 +43,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/m8_uf2_projecte_firebase_jxx_wrk/SignUp.java
@@ -0,0 +1,75 @@
+package com.example.m8_uf2_projecte_firebase_jxx_wrk;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.m8_uf2_projecte_firebase_jxx_wrk.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUp extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+    private TextInputEditText editTextEmail, editTextPassword;
+    private Button sign_up_btn;
+    private FirebaseAuth mAuth;
+    String email, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = (TextInputEditText) binding.tvEmail;
+        editTextPassword = (TextInputEditText) binding.tvPassword;
+        sign_up_btn = binding.signUpBtn;
+
+        if (editTextEmail.equals("")) {
+            Toast.makeText(SignUp.this, "The email cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (editTextPassword.equals("")) {
+            Toast.makeText(SignUp.this, "The password cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+
+    }
+}
