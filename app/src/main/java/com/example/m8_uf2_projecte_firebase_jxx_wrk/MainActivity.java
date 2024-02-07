d745df16976053eea801d9f60f82a6c1b52fae4c
jiins8 <117478765+jiins8@users.noreply.github.com>
2024-02-07 19:38:37 +0100

    version mas buena

--- app/src/main/java/com/example/m8_uf2_projecte_firebase_jxx_wrk/MainActivity.java
+++ app/src/main/java/com/example/m8_uf2_projecte_firebase_jxx_wrk/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.m8_uf2_projecte_firebase_jxx_wrk.databinding.ActivityMainBinding;
@@ -16,7 +17,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private FirebaseAuth auth;
-    private Button logOut_button;
+    private ImageButton logOut_button;
     private TextView user_details;
     FirebaseUser user;
 
